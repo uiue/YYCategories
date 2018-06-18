5ed3fe408413e9eca321eb98be1439fffc446b5c
Tiger <lianghaihu.dev@gmail.com>
2018-06-18 20:19:16 +0800

    Update UIScrollView+YYAdd.m

--- YYCategories/UIKit/UIScrollView+YYAdd.m
+++ YYCategories/UIKit/UIScrollView+YYAdd.m
@@ -17,41 +17,41 @@ YYSYNTH_DUMMY_CLASS(UIScrollView_YYAdd)
 
 @implementation UIScrollView (YYAdd)
 
-- (void)scrollToTop {
+- (void)YYscrollToTop {
     [self scrollToTopAnimated:YES];
 }
 
-- (void)scrollToBottom {
+- (void)YYscrollToBottom {
     [self scrollToBottomAnimated:YES];
 }
 
-- (void)scrollToLeft {
+- (void)YYscrollToLeft {
     [self scrollToLeftAnimated:YES];
 }
 
-- (void)scrollToRight {
+- (void)YYscrollToRight {
     [self scrollToRightAnimated:YES];
 }
 
-- (void)scrollToTopAnimated:(BOOL)animated {
+- (void)YYscrollToTopAnimated:(BOOL)animated {
     CGPoint off = self.contentOffset;
     off.y = 0 - self.contentInset.top;
     [self setContentOffset:off animated:animated];
 }
 
-- (void)scrollToBottomAnimated:(BOOL)animated {
+- (void)YYscrollToBottomAnimated:(BOOL)animated {
     CGPoint off = self.contentOffset;
     off.y = self.contentSize.height - self.bounds.size.height + self.contentInset.bottom;
     [self setContentOffset:off animated:animated];
 }
 
-- (void)scrollToLeftAnimated:(BOOL)animated {
+- (void)YYscrollToLeftAnimated:(BOOL)animated {
     CGPoint off = self.contentOffset;
     off.x = 0 - self.contentInset.left;
     [self setContentOffset:off animated:animated];
 }
 
-- (void)scrollToRightAnimated:(BOOL)animated {
+- (void)YYscrollToRightAnimated:(BOOL)animated {
     CGPoint off = self.contentOffset;
     off.x = self.contentSize.width - self.bounds.size.width + self.contentInset.right;
     [self setContentOffset:off animated:animated];
